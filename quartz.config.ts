e87bf78a079d562863a6b9a55edb8822b0daa714
Jason Chung <qazwsx5219430630@gmail.com>
2025-04-29 01:14:07 +0800

    Quartz sync: Apr 29, 2025, 1:14 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "qazwsx521943.github.io/jason-blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
